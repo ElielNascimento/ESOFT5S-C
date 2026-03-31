4ec465f094cc9e2ef2475cbd89962d0d282971ec
Danilo Mori <danilomori@Danilos-MacBook-Air.local>
2026-03-30 22:19:17 -0300

    repo

--- src/main/java/danilo/entity/repository/DaniloRepository.java
+++ src/main/java/danilo/entity/repository/DaniloRepository.java
@@ -1,4 +1,9 @@
 package danilo.entity.repository;
 
-public interface DaniloRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DaniloRepository extends JpaRepository {
+
 }
